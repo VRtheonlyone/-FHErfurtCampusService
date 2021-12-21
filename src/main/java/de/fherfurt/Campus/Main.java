6b157c7e27283140d210665c59b1c799b639795f
Nikita Studenikin <nikita.studenikin@cl.team>
2021-12-21 13:10:18 +0100

    searchForResults() function added and tested with Main

--- src/main/java/de/fherfurt/Campus/Main.java
+++ src/main/java/de/fherfurt/Campus/Main.java
@@ -1,4 +1,14 @@
 package de.fherfurt.Campus;
+import java.lang.*;
 
 public class Main {
+
+    public static final String LOCATION = "Location";
+    public static final String BUILDING = "Building";
+    public static final String ROOM = "Room";
+
+    public static void main(String[] args) {
+        Object Search = new Search();
+        System.out.println(de.fherfurt.Campus.Search.searchForResults("1", ROOM));
+    }
 }
